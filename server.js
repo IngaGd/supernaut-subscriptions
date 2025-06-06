94f0d00fc000062ad9e32f2a02c292dd99d91ab2
IngaGd <inga.gudaite@gmail.com>
2025-06-06 14:52:43 +0300

    Add JSON body parsers and routes

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/", require("./routes/accessCheck"));
+app.use("/", require("./routes/webhook"));
 
 app.listen(PORT, () => {
   console.log(`Server is started on port: ${PORT}`);
